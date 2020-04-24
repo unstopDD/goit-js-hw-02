0c09ab54df72012d3a906d47d974bb37832e110b
Dmitriy Khomiakov <unstopddd@gmail.com>
2020-04-24 21:19:46 +0300

    Add changes ex-01 and ex-06

--- excercise-01/index.js
+++ excercise-01/index.js
@@ -2,8 +2,7 @@
 
 const logItems = function(array) {
   for (let i = 0; i < array.length; i += 1) {
-    // eslint-disable-next-line
-    console.log(i + 1 + ' - ' + array[i]);
+    console.log(`${i + 1} - ${array[i]}`);
   }
 };
 
--- excercise-06/index.js
+++ excercise-06/index.js
@@ -6,15 +6,18 @@ let total = 0;
 
 do {
   input = prompt(`Введите число`);
-  if (isNaN(input)) {
+  if (Number.isNaN(input)) {
     alert('Было введено не число, попробуйте еще раз');
   } else {
     const inputNumber = Number(input);
     numbers.push(inputNumber);
   }
 } while (input !== null);
-for (let i = 0; i < numbers.length; i += 1) {
-  total += numbers[i];
+
+if (numbers.length > 0) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    total += numbers[i];
+  }
 }
 
 alert(`Общая сумма чисел равна ${total}`);
